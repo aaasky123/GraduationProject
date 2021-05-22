c2c87de6baa543832b20a4dbe8c839f859a7e096
aaasky123 <44549386+aaasky123@users.noreply.github.com>
2021-05-22 09:05:54 +0800

    Update WebMvcConfig.java

--- work/src/main/java/com/wxj/work/util/WebMvcConfig.java
+++ work/src/main/java/com/wxj/work/util/WebMvcConfig.java
@@ -7,6 +7,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 public class WebMvcConfig  extends WebMvcConfigurerAdapter {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/image/**").addResourceLocations("file:D:\\graduationbackup\\");
+        registry.addResourceHandler("/image/**").addResourceLocations("file:/uploadPhoto/");
     }
 }
